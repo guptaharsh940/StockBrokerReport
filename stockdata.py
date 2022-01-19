fc846c16a9e3f9481e2eed1665bb84a2950983b4
Harsh Gupta <guptaharsh940@gmail.com>
2022-01-20 01:29:59 +0530

    Start of getting NSE data

--- stockdata.py
+++ stockdata.py
@@ -1,1 +1,23 @@
 import nsepy
+import pynse
+import os
+import requests
+def getData(name):
+    try:
+        os.system('node "D:\Files\Code\Stock Market\stock-market-india"')
+        api_url = f'http://localhost:3000/nse/search_stocks?keyword={name}'
+    except:
+        return False
+        pass
+
+    
+    data = requests.get(api_url).json()
+    for i in range (len(data)):
+        print(data[i]['symbol'])
+z= True
+while z == True:
+    a = input("Enter Name - ")
+    getData(a)
+    b = input("Press Enter to continue...")
+    if b != "":
+        z=False
