a9f003bb4f24cdb91be719012d17528775fb8e1a
Harsh Gupta <guptaharsh940@gmail.com>
2022-01-23 09:37:19 +0530

    Working Stock Symbol Retreive and added the maanavshah API in local folder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import math
-from stockdata import get_stock_symbol
+import stockdata
 data = pd.read_csv('data.csv')
 print(data)
 sno = data['S No.'].max()
@@ -12,18 +12,27 @@ if math.isnan(sno):
 # import callretriever
 # callretriever
 def getReco(sno):
+    '''Gets the Recommendation from RecoWebsiteScrape and then checks for Redundancy in the database and if
+    new data is found it adds to the database. It recieves the max sno of the database as argument.
+    '''
     global data
+    sno_to_get = set()
     # print(data)
     import RecoWebsiteScrape
     reco = list(RecoWebsiteScrape.titles)
     for item in reco:
-        if item in data.values:
+        if reco[-1] == item and (item in data.values):
+            addSymboltoOld()
+            continue
+
+        elif item in data.values:
             continue
         '''Initiating a temporary dictionary to add the data to'''
         keys = ['S No.','Reco Full','Reco Date','Reco Time','Reco Provider','Reco For','Target Price','Action',]
         RecoDict = dict(zip(keys, [None]*len(keys)))
         
         RecoDict['S No.']=(sno + 1)
+        sno_to_get.add(sno+1)
         RecoDict['Reco Full']=(item)
         RecoDict['Reco Date']=(time.strftime("%d/%m/%Y",time.localtime()))
         RecoDict['Reco Time']=(time.strftime("%H:%M:%S",time.localtime()))
@@ -48,17 +57,64 @@ def getReco(sno):
         data = data.append(RecoDict,ignore_index=True)
         print(RecoDict)
         print(data)
-    
+    compsym = getSymbol(sno_to_get)
+    for i in compsym.keys():
+        a = data['S No.'].loc[lambda x:x==float(i)].index
+
+        # data.insert(a,'Symbol', compsym[i])
+        data.Symbol[a] = compsym[i]
+
     
     data.to_csv('data.csv',index=False)
 
+def getSymbol(sno):
+    global data
+    '''Gets the company name from Serial No.'''
+    companyname = []
+    for i in list(sno):
+        a = list(data['S No.'].loc[lambda x:x==float(i)].index)
+        for y in a:
+            companyname.append(data.iloc[y]['Reco For'])
+    values = stockdata.start(companyname)
+    companysym = {list(sno)[i]: values[i] for i in range(len(list(sno)))}
+    print(companysym)
+    return companysym
+
+        # print(companyname)
+
+def addSymboltoOld():
+    global data
+    sno = set()
+    a = 0
+    checkfornan = data['Symbol'].isnull()
+    print(checkfornan)
+    for x in checkfornan:
+        a+=1
+        if x:
+            sno.add(a)
+        else:
+            continue
+    print(sno)
+    companyname = []
+    for i in list(sno):
+        a = list(data['S No.'].loc[lambda x:x==float(i)].index)
+        for y in a:
+            companyname.append(data.iloc[y]['Reco For'])
+    values = stockdata.start(companyname)
+    compsym = {list(sno)[i]: values[i] for i in range(len(list(sno)))}
+    for i in compsym.keys():
+        a = data['S No.'].loc[lambda x:x==float(i)].index
+
+        # data.insert(a,'Symbol', compsym[i])
+        data.Symbol[a] = compsym[i]
+        # data.loc[a,6] = compsym[i]
+
 
             
-            
 
             
-
-
+# while True:
 getReco(sno)
-t = time.strftime("%H:%M:%S",time.localtime())
+# getSymbol({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23})
+# t = time.strftime("%H:%M:%S",time.localtime())
 # print(t)
--- stockdata.py
+++ stockdata.py
@@ -1,79 +1,99 @@
 from py_compile import _get_default_invalidation_mode
 from sys import api_version
-import nsepy
-import pynse
-import os
-import sys
+# import nsepy
+# import pynse
+# import os
+# import sys
 import requests
 import time
 import subprocess as sub
 import threading
 
 data = None
-status = True
+companysymbol = []
+# status = True
 timeout = 5
+nodepro = None
 def run_server():
-    global status
+    # global status
     global timeout
+    global nodepro
     print("This has started")
     try:
-        sub.run('node "D:\Files\Code\Stock Market\stock-market-india"',timeout=timeout)
+        nodepro = sub.Popen('node "stock-market-india"')
     except:
         print("Server Timed Out")    
+    
     # sys.exit()
 
-def get_stock_symbol(name):
+def get_stock_symbol(clist):
+    '''Takes Company Name List and add corresponding symbol to the list'''
     global data
-    print("I am here")
-    api_url = f'http://localhost:3000/nse/search_stocks?keyword={name}'
-    try:
-        data = requests.get(api_url).json()
-    except:
-        print("This didn't work")
-        run_server()
-        pass
-    if len(data) == 0:
-        print("Not found")
-    else:
-        for i in range (len(data)):
-            print(data[i]['symbol'])
-    print("This has ended")
+    global companysymbol
+    global timeout
+    timeout = (len(clist) * 2) + 5
+    for name in clist:
+    # print("I am here")
+        api_url = f'http://localhost:3000/nse/search_stocks?keyword={name}'
+        try:
+            data = requests.get(api_url).json()
+        except:
+            print("This didn't work")
+            companysymbol.append('N/A')
+            run_server()
+            continue
+            # pass
+        if len(data) == 0:
+            print("Not found")
+        else:
+            for i in range (len(data)):
+                if 'Mutual Fund' in data[i]['name'] or 'ETF' in data[i]['name']:
+                    continue
+                elif (i==len(data) - 1) and (name.split()[0] not in data[i]['name']):
+                    companysymbol.append('N/A')
+                    break
+                elif name.split()[0] not in data[i]['name']:
+                    continue
+                else:
+                    companysymbol.append(data[i]['symbol'])
+                    print(data[i]['symbol'])
+                    break
     
+    print("This has ended")
+    timeout = 1
 
 def timecheck():
     global timeout
-    global status
-    print(timeout)
+    # global status
+    global nodepro
+    # print(timeout)
     # timeout = 10
     timeout_start = time.time()
     while True:
-        print("This is timing")
+        # print("This is timing")
         if time.time() > timeout_start + timeout:    
             print("Timed Out")
+            nodepro.kill()
             break
             
-        time.sleep(5)
+        time.sleep(2)
         
-
-
-    
-    
-z= True
-while z == True:
-    a = input("Enter Name - ")
+def start(a):
+    global companysymbol
+    if len(a) == 0:
+        return False
+    else:
     # if __name__ == '__main__':
-    runserver = threading.Thread(target=run_server)
-    timemanage = threading.Thread(target=timecheck)
-    datacollector = threading.Thread(target=get_stock_symbol,args=(a,))
-    # get_data = threading.Thread(target=getData())
-    runserver.start()
-    timemanage.start()
-    datacollector.start()
-    # get_data.start()
-    runserver.join()
-    timemanage.join()
-    datacollector.join()
-    # get_data.join()
-    b = input("Press Enter to continue...")
-    if b != "":
-        z=False
+        runserver = threading.Thread(target=run_server)
+        timemanage = threading.Thread(target=timecheck)
+        datacollector = threading.Thread(target=get_stock_symbol,args=(a,))
+        # get_data = threading.Thread(target=getData())
+        runserver.start()
+        timemanage.start()
+        datacollector.start()
+        # get_data.start()
+        runserver.join()
+        timemanage.join()
+        datacollector.join()
+        # get_data.join()
+        return companysymbol
